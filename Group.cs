3d2d90d02d86d32dfc4713b09972536277517079
opilane <opilane@HP-CZC2349HTC>
2023-09-05 16:23:17 +0300

    Add project files.

---
+++ Group.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Liik
+{
+    public string Name { get; }
+    public int Age { get; }
+    public DateTime RegistrationDate { get; }
+
+    public Liik(string name, int age, DateTime registrationDate)
+    {
+        Name = name;
+        Age = age;
+        RegistrationDate = registrationDate;
+    }
+}
+public class Group
+{
+    private List<Liik> liikmed = new List<Liik>();
+    private readonly int maxAmount;
+
+    public List<Liik> Liikmed
+    {
+        get { return liikmed; }
+    }
+
+    public Group(int maxAmount)
+    {
+        this.maxAmount = maxAmount;
+    }
+
+    public bool AddOpilane(string name, int age, DateTime registrationDate)
+    {
+        if (liikmed.Any(liik => liik.Name == name)) return false;
+        if (liikmed.Count >= maxAmount) return false;
+        liikmed.Add(new Liik(name, age, registrationDate));
+        return true;
+    }
+
+    public int GetOpilasedCount()
+    {
+        return liikmed.Count;
+    }
+
+    public bool HasOpilane(string name)
+    {
+        return liikmed.Any(liik => liik.Name == name);
+        
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Group group = new Group(3);
+        PrintResult(group.AddOpilane("John", 29, DateTime.Now));
+        PrintResult(group.AddOpilane("John", 30, DateTime.Now));
+        PrintResult(group.AddOpilane("Mary", 42, DateTime.Now));
+        PrintResult(group.AddOpilane("Jane", 28, DateTime.Now));
+        PrintResult(group.AddOpilane("Joseph", 34, DateTime.Now));
+        PrintOpilased(group);
+
+        Group bookClub = new Group(4);
+        PrintResult(bookClub.AddOpilane("Albert", 29, DateTime.Now));
+        PrintResult(bookClub.AddOpilane("Samantha", 21, DateTime.Now));
+        PrintResult(bookClub.AddOpilane("Robert", 30, DateTime.Now));
+        PrintResult(bookClub.AddOpilane("Roberta", 31, DateTime.Now));
+        PrintOpilased(bookClub);
+
+        Console.WriteLine("Albert exists in bookClub: " + bookClub.HasOpilane("Albert"));
+        Console.WriteLine("John exists in bookClub: " + bookClub.HasOpilane("John"));
+        Console.ReadLine();
+    }
+
+    public static void PrintResult(bool result)
+    {
+        if (result)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("True");
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("False");
+        }
+        Console.ResetColor();
+    }
+
+    public static void PrintOpilased(Group group)
+    {
+        Console.WriteLine("Opilased:");
+        if (group.Liikmed.Count == 0)
+        {
+            Console.WriteLine("Grupis pole opilasi.");
+            return;
+        }
+        Liik oldestOpilane = group.Liikmed[0];
+        foreach (var opilane in group.Liikmed)
+        {
+            if (opilane.Age > oldestOpilane.Age)
+            {
+                oldestOpilane = opilane;
+            }
+        }
+        foreach (var opilane in group.Liikmed)
+        {
+            Console.ForegroundColor = opilane == oldestOpilane ? ConsoleColor.Yellow : ConsoleColor.White;
+            Console.WriteLine($"{opilane.Name}, {opilane.Age} aastat, {opilane.RegistrationDate}, {group.HasOpilane(opilane.Name)}");
+            Console.ResetColor();
+        }
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"Kõige vanem opilane grupis: {oldestOpilane.Name}, {oldestOpilane.Age} aastat");
+        Console.ResetColor();
+    }
+}
